439222f411cbb6421f1975adab47f2100fdd07db
Abraham Toriz Cruz <awonderfulcode@gmail.com>
2016-08-14 18:45:51 -0500

    attempt to send notification to device when video is ready

--- detect.py
+++ detect.py
@@ -3,6 +3,7 @@ from gpiozero import MotionSensor, LED
 from picamera import PiCamera
 from datetime import datetime
 from time import sleep, time
+from pushover import send_notification
 
 pir    = MotionSensor(4)
 camera = PiCamera()
@@ -18,3 +19,9 @@ while True:
     camera.stop_recording()
     led.off()
     print(' '*len(filename), end='\r', flush=True)
+    send_notification(
+        title = 'New video recorded',
+        message = 'Your raspberry has recorded a new video',
+        url = 'http://192.168.1.72/',
+        url_title = 'Go to the video',
+    )
---
+++ pushover.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import requests
+import settings
+
+def send_notification(title='Title', message='message', url=None, url_title=None):
+    r = requests.post('https://api.pushover.net/1/messages.json', data={
+        'token'     : settings.TOKEN,
+        'user'      : settings.USER_KEY,
+        'title'     : title,
+        'message'   : message,
+        'url'       : url,
+        'url_title' : url_title,
+    })
+    if r.status_code != 200:
+        raise Exception('Failed request with status {}'.format(r.status_code))
+
+if __name__ == '__main__':
+    send_notification(
+        title='From python',
+        message='hello',
+        url='http://192.168.1.72',
+        url_title='Go to video'
+    )
