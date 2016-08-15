3bee7f1bc66939a2c01dcac2b757a1eebe39e5c9
Abraham Toriz Cruz <awonderfulcode@gmail.com>
2016-08-14 20:02:22 -0500

    wrap video after recording in a mp4 container

--- detect.py
+++ detect.py
@@ -4,26 +4,32 @@ from picamera import PiCamera
 from datetime import datetime
 from time import sleep, time
 from pushover import send_notification
+import subprocess
 
 pir    = MotionSensor(4)
 camera = PiCamera()
 led    = LED(17)
 
+print('Waiting for sone aminal...')
+
 while True:
     pir.wait_for_motion()
-    filename = datetime.now().strftime('%Y-%m-%d_%H.%M.%S.h264')
+    filename = datetime.now().strftime('%Y-%m-%d_%H.%M.%S')
     print(filename, end='\r', flush=True)
 
     led.on()
-    camera.start_recording('videos/'+filename)
+    camera.start_recording('videos/{}.h264'.format(filename))
     sleep(30) # Record for 30 secs
     camera.stop_recording()
     led.off()
     print(' '*len(filename), end='\r', flush=True)
 
+    # Wrap the file into mp4 so i can view it
+    subprocess.check_call(['MP4Box', '-add', 'videos/{}.h264'.format(filename), 'videos/{}.mp4'.format(filename)])
+
     send_notification(
         title = 'New video recorded',
         message = 'Your raspberry has recorded a new video',
-        url = 'http://192.168.1.72/animal-detector/{}'.format(filename),
+        url = 'http://192.168.1.72/animal-detector/{}.mp4'.format(filename),
         url_title = 'Go to the video',
     )
