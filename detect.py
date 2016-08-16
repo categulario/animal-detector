12f9cb4befd7262e92bc4bcb503b1987ebb952f0
Abraham Toriz Cruz <awonderfulcode@gmail.com>
2016-08-16 07:12:24 -0500

    add a graceful kill handler

--- detect.py
+++ detect.py
@@ -5,32 +5,54 @@ from datetime import datetime
 from time import sleep, time
 from pushover import send_notification
 import subprocess
+import signal
 
+# Setup some io
 pir    = MotionSensor(4)
 camera = PiCamera()
 led    = LED(17)
 
-print('Waiting for some aminal...', flush=True)
+class GracefulKiller:
+  kill_now = False
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
 
-while True:
-    pir.wait_for_motion()
-    filename = datetime.now().strftime('%Y-%m-%d_%H.%M.%S')
-    print('Recording {}'.format(filename), flush=True)
+  def exit_gracefully(self,signum, frame):
+    self.kill_now = True
 
-    led.on()
-    camera.start_recording('videos/{}.h264'.format(filename))
-    sleep(30) # Record for 30 secs
-    camera.stop_recording()
-    led.off()
+def main(killer):
+    while True:
+        if killer.kill_now:
+            break
+        pir.wait_for_motion(1)
+        if not pir.motion_detected:
+            # no motion, the wait process stopped by timeout
+            continue
+        filename = datetime.now().strftime('%Y-%m-%d_%H.%M.%S')
+        print('Recording {}'.format(filename), flush=True)
 
-    print('Encoding {}'.format(filename), flush=True)
-    # Wrap the file into mp4 so i can view it
-    subprocess.check_call(['MP4Box', '-add', 'videos/{}.h264'.format(filename), 'videos/{}.mp4'.format(filename)])
+        led.on()
+        camera.start_recording('videos/{}.h264'.format(filename))
+        sleep(30) # Record for 30 secs
+        camera.stop_recording()
+        led.off()
 
-    print('Notify about {}'.format(filename), flush=True)
-    send_notification(
-        title = 'New video recorded',
-        message = 'Your raspberry has recorded a new video',
-        url = 'http://192.168.1.72/animal-detector/{}.mp4'.format(filename),
-        url_title = 'Go to the video',
-    )
+        print('Encoding {}'.format(filename), flush=True)
+        # Wrap the file into mp4 so i can view it
+        subprocess.check_call(['MP4Box', '-add', 'videos/{}.h264'.format(filename), 'videos/{}.mp4'.format(filename)])
+
+        print('Notify about {}'.format(filename), flush=True)
+        send_notification(
+            title = 'New video recorded',
+            message = 'Your raspberry has recorded a new video',
+            url = 'http://192.168.1.72/animal-detector/{}.mp4'.format(filename),
+            url_title = 'Go to the video',
+        )
+    print("Babay!")
+
+if __name__ == '__main__':
+    print('Waiting for some aminal...', flush=True)
+
+    killer = GracefulKiller()
+    main(killer)
