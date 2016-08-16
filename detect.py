6ac130d37eda5563dd89074cdaf1db0f1dea087f
Abraham Toriz Cruz <awonderfulcode@gmail.com>
2016-08-16 00:22:41 -0500

    an opinionated start script

--- detect.py
+++ detect.py
@@ -10,23 +10,24 @@ pir    = MotionSensor(4)
 camera = PiCamera()
 led    = LED(17)
 
-print('Waiting for sone aminal...')
+print('Waiting for some aminal...', flush=True)
 
 while True:
     pir.wait_for_motion()
     filename = datetime.now().strftime('%Y-%m-%d_%H.%M.%S')
-    print(filename, end='\r', flush=True)
+    print('Recording {}'.format(filename), flush=True)
 
     led.on()
     camera.start_recording('videos/{}.h264'.format(filename))
     sleep(30) # Record for 30 secs
     camera.stop_recording()
     led.off()
-    print(' '*len(filename), end='\r', flush=True)
 
+    print('Encoding {}'.format(filename), flush=True)
     # Wrap the file into mp4 so i can view it
     subprocess.check_call(['MP4Box', '-add', 'videos/{}.h264'.format(filename), 'videos/{}.mp4'.format(filename)])
 
+    print('Notify about {}'.format(filename), flush=True)
     send_notification(
         title = 'New video recorded',
         message = 'Your raspberry has recorded a new video',
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./detect.py >> detect.log 2>> error.log &
